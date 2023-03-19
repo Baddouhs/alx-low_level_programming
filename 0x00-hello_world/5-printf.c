2f5a8789b05693858083d0f905b9b6fba4a97779
Sara <baddouhs@gmail.com>
2023-03-19 06:51:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - this is the main function
  *
- * return: 0 when successful
+ * return: 0 when successfull
  */
 
-int main(void)
+int	main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
