0b6003e583f0b99f6de9b0582ad91d84b88e43af
Sara <baddouhs@gmail.com>
2023-04-02 07:06:18 -0700

    0x01-variable_if_else_while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
+ * main - prints all possible combinations of single digit numbers
  * Return: Always 0 (Success)
  */
 
@@ -11,9 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		printf("%d, i);
+		putchar(i);
+		if (i != 57);
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
